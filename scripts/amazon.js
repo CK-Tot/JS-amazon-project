57deb92bd4ef8a468466e9dcafa70d552a058abf
CK-Tot <changkuothtot40@gmail.com>
2025-10-29 21:50:03 -0500

    feat: Add add to cart functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,18 +1,78 @@
+const productGrid = document.querySelector('.js-products-grid');
 
-// let productsHTML = '';
+function renderProducts (products) {
+    productGrid.innerHTML = '';
 
-// products.forEach((product) => {
-//    productsHTML += `
-//     <div class="product-container">
-//             <div class="product-image-container">
-//                 <img class="product-image"
-//                 src="${product.image}">
-//             </div>
+    const productHTML = products.map(product => {
+        return `
+                <div class="product-container">
 
-//             <div class="product-name limit-text-to-2-lines">
-//                 ${product.name}
-//             </div>
+                    <div class="product-image-container">
+                        <img class="product-image" src="${product.image}" />
+                    </div>
 
+                    <div class="product-name limit-text-to-2-lines">
+                        ${product.name}
+                    </div>
+
+                    <div class="product-rating-container">
+                        <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png" />
+                        <div class="product-rating-count link-primary">
+                            ${product.rating.count}
+                        </div>
+                    </div>
+
+                    <div class="product-price">
+                    $${(product.priceCents / 100).toFixed(2)}
+                    </div>
+
+                    <div class="product-quantity-container">
+                        <select>
+                            <option value="1" selected>1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
+
+                     <div class="product-spacer"></div>
+
+                     <div class="added-to-cart">
+                        <img src="images/icons/checkmark.png">
+                        Added
+                     </div>
+
+                     <button data-product-name="${product.name}" class="add-to-cart-button button-primary js-add-to-cart"> Add to Cart</button>
+
+                </div>
+        `
+    }).join('');
+
+    productGrid.innerHTML = productHTML;
+
+    // Add to cart function
+    const addToCart = document.querySelectorAll('.js-add-to-cart');
+
+    addToCart.forEach(btn => {
+        btn.addEventListener('click', () => {
+            console.log(btn.dataset.productName);
+        })
+    })
+
+
+}
+
+renderProducts(products);
+
+
+// <div class="product-container">
+//             
 //             <div class="product-rating-container">
 //                 <img class="product-rating-stars"
 //                 src="images/ratings/rating-${product.rating.stars * 10}.png">
@@ -22,7 +82,7 @@
 //             </div>
 
 //             <div class="product-price">
-//                 $${(product.price / 100).toFixed(2)}
+//                 $${(product.priceCents / 100).toFixed(2)}
 //             </div>
 
 //             <div class="product-quantity-container">
@@ -43,79 +103,11 @@
 //             <div class="product-spacer"></div>
 
 //             <div class="added-to-cart">
-//                 <img src="images/icons/checkmark.png">
-//                 Added
+//                 
 //             </div>
 
-//             <button class="add-to-cart-button button-primary">
+//             <button data-product-name="${product.name}" class="add-to-cart-button button-primary js-add-to-cart">
 //                 Add to Cart
 //             </button>
 //             </div>` ;
 
-// })
-
-const productContainer = document.querySelector('.js-products-grid');
-
-const renderProducts = products => {
-
-    const productsHTML = products.map(product => {
-        return  `
-    <div class="product-container">
-            <div class="product-image-container">
-                <img class="product-image"
-                src="${product.image}">
-            </div>
-
-            <div class="product-name limit-text-to-2-lines">
-                ${product.name}
-            </div>
-
-            <div class="product-rating-container">
-                <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars * 10}.png">
-                <div class="product-rating-count link-primary">
-                ${product.rating.count}
-                </div>
-            </div>
-
-            <div class="product-price">
-                $${(product.price / 100).toFixed(2)}
-            </div>
-
-            <div class="product-quantity-container">
-                <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                </select>
-            </div>
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart">
-                <img src="images/icons/checkmark.png">
-                Added
-            </div>
-
-            <button class="add-to-cart-button button-primary">
-                Add to Cart
-            </button>
-            </div>` ;
-
-    }).join('');
-
-    productContainer.innerHTML = productsHTML;
-
-
-}
-
-
-
-renderProducts(products);
